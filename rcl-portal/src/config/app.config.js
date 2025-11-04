91f26e97234da416454424eb460f80ecd5883b3b
RotaractInRID3220 <digitalservices.rid3220@gmail.com>
2025-11-04 21:39:45 +0530

    Update app.config.js

--- rcl-portal/src/config/app.config.js
+++ rcl-portal/src/config/app.config.js
@@ -24,7 +24,7 @@ export const getSportDayFromPlace = (place) => {
 export const APP_CONFIG = {
   GENERAL_MEMBER_WARNING_THRESHOLD: 70, // % threshold for PieStatCard warning
   REGISTRATION_OPENING_DATE: '2025-10-19T10:00:00', // Date and time from which registration is enabled (YYYY-MM-DDTHH:mm:ss)
-  REGISTRATION_DEADLINE: '2025-11-05T00:00:00', // Date and time after which registration/deletion is disabled (YYYY-MM-DDTHH:mm:ss)
+  REGISTRATION_DEADLINE: '2025-11-05T00:30:00', // Date and time after which registration/deletion is disabled (YYYY-MM-DDTHH:mm:ss)
   CURRENT_SPORT_DAY: SPORT_DAYS.E_SPORT.value, // Current sport day for on-the-day registrations
   MEMBERSHIP_CUTOFF_DATE: '2025-10-05T00:30:00.000Z', // Set cutoff date as 'YYYY-MM-DDTHH:mm:ss' or leave empty for current date
   REGISTRATION_FEE: 800, // Registration fee per player in Rs.
